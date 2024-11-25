a11e4450edd270505c4b7b92a47507823848e1f3
Mateusz Nalepa <mateuszcholyn@gmail.com>
2024-11-25 10:59:43 +0100

    init

---
+++ src/main/kotlin/com/mateusz/nalepa/communicationerrors/CommunicationerrorsApplication.kt
@@ -0,0 +1,11 @@
+package com.mateusz.nalepa.communicationerrors
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CommunicationerrorsApplication
+
+fun main(args: Array<String>) {
+	runApplication<CommunicationerrorsApplication>(*args)
+}
---
+++ src/test/kotlin/com/mateusz/nalepa/communicationerrors/CommunicationerrorsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.mateusz.nalepa.communicationerrors
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CommunicationerrorsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
