a555ff1d6b06e8d13f229cab70e7a7a3542894c2
Mateusz Nalepa <mateuszcholyn@gmail.com>
2024-12-09 13:26:18 +0100

    add bulkhead

--- src/test/kotlin/com/mateusz/nalepa/communicationerrors/bulkhead/BulkheadTest.kt
+++ src/test/kotlin/com/mateusz/nalepa/communicationerrors/bulkhead/BulkheadTest.kt
@@ -2,10 +2,7 @@ package com.mateusz.nalepa.communicationerrors.bulkhead
 
 import org.junit.jupiter.api.Test
 import java.time.Duration
-import java.util.concurrent.CompletableFuture
-import java.util.concurrent.Executors
-import java.util.concurrent.Future
-import java.util.concurrent.TimeUnit
+import java.util.concurrent.*
 
 class BulkheadTest {
 
@@ -78,6 +75,89 @@ class BulkheadTest {
         Thread.sleep(Duration.ofSeconds(20))
     }
 
+    @Test
+    fun `allow execute right now, without queue`() {
+        // Liczba wątków w puli
+        val corePoolSize = 10
+        val maximumPoolSize = 10
+        val keepAliveTime = 10L
+        val unit = TimeUnit.SECONDS
+
+        val waitingTaskQueue = SynchronousQueue<Runnable>()
+
+        val executor = ThreadPoolExecutor(
+            corePoolSize,
+            maximumPoolSize,
+            keepAliveTime,
+            unit,
+            waitingTaskQueue,
+            ThreadPoolExecutor.AbortPolicy() // what to do when queue is full
+        )
+
+        val futuresSlow = mutableListOf<Future<String>>()
+        val measureStart = System.currentTimeMillis()
+
+
+        (1..30).map {
+            try {
+                futuresSlow.add(CompletableFuture.supplyAsync({ SlowExternalService.longRunningTask(it) }, executor))
+            } catch (t: RejectedExecutionException) {
+                println("Every thread is working right now. Unable to process: $it")
+            }
+        }
+
+        // then
+        val threadForSlow = Thread {
+            futuresSlow.forEach { println(it.get(1, TimeUnit.DAYS)) }
+            val duration = Duration.ofMillis(System.currentTimeMillis() - measureStart)
+            println("SLOW Processing took: $duration")
+        }
+        threadForSlow.start()
+        Thread.sleep(Duration.ofSeconds(20))
+    }
+
+
+    @Test
+    fun `allow only 10 tasks in queue`() {
+        // Liczba wątków w puli
+        val corePoolSize = 10
+        val maximumPoolSize = 10
+        val keepAliveTime = 10L
+        val unit = TimeUnit.SECONDS
+        val queueSize = 10
+
+        val waitingTaskQueue = ArrayBlockingQueue<Runnable>(queueSize)
+
+        val executor = ThreadPoolExecutor(
+            corePoolSize,
+            maximumPoolSize,
+            keepAliveTime,
+            unit,
+            waitingTaskQueue,
+            ThreadPoolExecutor.AbortPolicy() // what to do when queue is full
+        )
+
+        val futuresSlow = mutableListOf<Future<String>>()
+        val measureStart = System.currentTimeMillis()
+
+
+        (1..30).map {
+            try {
+                futuresSlow.add(CompletableFuture.supplyAsync({ SlowExternalService.longRunningTask(it) }, executor))
+            } catch (t: RejectedExecutionException) {
+                println("Queue is full. Unable to process: $it")
+            }
+        }
+
+        // then
+        val threadForSlow = Thread {
+            futuresSlow.forEach { println(it.get(1, TimeUnit.DAYS)) }
+            val duration = Duration.ofMillis(System.currentTimeMillis() - measureStart)
+            println("SLOW Processing took: $duration")
+        }
+        threadForSlow.start()
+        Thread.sleep(Duration.ofSeconds(20))
+    }
 
 }
 
